6c11e068dd36692735ee1587ac6aeff577dbd2c5
Tom Pesman <tom@tnux.net>
2014-05-27 11:36:36 +0200

    removed feedback_poll from apns config

--- lib/pushr/configuration_apns.rb
+++ lib/pushr/configuration_apns.rb
@@ -1,10 +1,9 @@
 module Pushr
   class ConfigurationApns < Pushr::Configuration
     attr_accessor :id, :type, :app, :enabled, :connections, :certificate, :certificate_password,
-                  :sandbox, :feedback_poll, :skip_check_for_error
+                  :sandbox, :skip_check_for_error
     validates :certificate, presence: true
     validates :sandbox, inclusion: { in: [true, false] }
-    validates :feedback_poll, numericality: true, presence: true
     validates :skip_check_for_error, inclusion: { in: [true, false] }, allow_blank: true
 
     def name
@@ -13,8 +12,7 @@ module Pushr
 
     def to_json
       hsh = { type: self.class.to_s, app: app, enabled: enabled, connections: connections, certificate: certificate,
-              certificate_password: certificate_password, sandbox: sandbox, feedback_poll: feedback_poll,
-              skip_check_for_error: skip_check_for_error }
+              certificate_password: certificate_password, sandbox: sandbox, skip_check_for_error: skip_check_for_error }
       MultiJson.dump(hsh)
     end
   end
